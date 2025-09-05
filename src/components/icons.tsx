09f8dabdb1eb74c0f60733489d077b5a88989811
Krishna Baviskar <eventomatic.team@gmail.com>
2025-09-05 20:19:12 +0000

    deccrease the sixe of logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,8 +5,8 @@ export function Logo(props: Omit<React.ComponentProps<typeof Image>, 'src' | 'al
     <Image
       src="/nexmov.png"
       alt="NexMov.ai Logo"
-      width={128}
-      height={32}
+      width={80}
+      height={20}
       priority
       {...props}
     />
