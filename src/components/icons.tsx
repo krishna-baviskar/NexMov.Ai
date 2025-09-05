d6740bd6943b5716db542c947d2c170257101ef1
Krishna Baviskar <eventomatic.team@gmail.com>
2025-09-05 20:03:40 +0000

    i want here nexmov.png which have have given in app directory (_for elem

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,20 +1,14 @@
+import Image from "next/image";
 import type { SVGProps } from "react";
 
-export function Logo(props: SVGProps<SVGSVGElement>) {
+export function Logo(props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+    <Image
+      src="/nexmov.png"
+      alt="NexMov.ai Logo"
+      width={32}
+      height={32}
       {...props}
-    >
-      <path d="M12 2L2 7l10 5 10-5-10-5z" />
-      <path d="M2 17l10 5 10-5" />
-      <path d="M2 12l10 5 10-5" />
-    </svg>
+    />
   );
 }
