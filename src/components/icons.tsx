b6fff28b6da377392b989cbe7e7ca5480ce7804d
Krishna Baviskar <eventomatic.team@gmail.com>
2025-09-08 18:27:11 +0000

    decrease the size of logo  (_for element img_)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,8 +5,8 @@ export function Logo(props: Omit<React.ComponentProps<typeof Image>, 'src' | 'al
     <Image
       src="/nexmov.png"
       alt="NexMov.ai Logo"
-      width={80}
-      height={20}
+      width={24}
+      height={24}
       priority
       {...props}
     />
