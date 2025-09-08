964144079fb4be287c7167d7f9e305936635898b
Krishna Baviskar <eventomatic.team@gmail.com>
2025-09-08 18:32:33 +0000

    no nexmov.png is seen here (_for element img_)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,15 +1,12 @@
-import Image from "next/image";
-
 export function Logo(
-  props: Omit<React.ComponentProps<typeof Image>, "src" | "alt">
+  props: Omit<React.ComponentProps<'img'>, 'src' | 'alt'>
 ) {
   return (
-    <Image
+    <img
       src="/nexmov.png"
       alt="NexMov.ai Logo"
-      width={32}
-      height={32}
-      priority
+      width={40}
+      height={40}
       {...props}
     />
   );
